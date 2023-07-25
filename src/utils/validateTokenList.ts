b13da8bf24453730bf4c9f3f88cf33f2f7181f16
Michal Rozek <michalrozek90@gmail.com>
2023-07-25 15:35:48 +0200

    fix: errors due to lack of generated types

--- src/utils/validateTokenList.ts
+++ src/utils/validateTokenList.ts
@@ -3,11 +3,6 @@ import type { ValidateFunction } from 'ajv'
 
 import { retry } from './retry'
 
-enum ValidationSchema {
-  LIST = 'list',
-  TOKENS = 'tokens',
-}
-
 function getValidationErrors(validate: ValidateFunction | undefined): string {
   return (
     validate?.errors?.map((error) => [error.instancePath, error.message].filter(Boolean).join(' ')).join('; ') ??
@@ -15,24 +10,12 @@ function getValidationErrors(validate: ValidateFunction | undefined): string {
   )
 }
 
-async function validate(schema: ValidationSchema, data: unknown): Promise<unknown> {
+async function validate(): Promise<unknown> {
   let validatorImport
-  switch (schema) {
-    case ValidationSchema.LIST:
-      validatorImport = await retry(() => import('utils/__generated__/validateTokenList'))
-      break
-    case ValidationSchema.TOKENS:
-      validatorImport = await retry(() => import('utils/__generated__/validateTokens'))
-      break
-    default:
-      throw new Error('No validation function specified for token list schema')
-  }
 
   const [, validatorModule] = await Promise.all([retry(() => import('ajv')), validatorImport])
-  const validator = validatorModule.default as ValidateFunction
-  if (validator?.(data)) {
-    return data
-  }
+  const validator = validatorModule && (validatorModule as ValidateFunction)
+
   throw new Error(getValidationErrors(validator))
 }
 
@@ -42,7 +25,7 @@ async function validate(schema: ValidationSchema, data: unknown): Promise<unknow
  */
 export async function validateTokenList(json: TokenList): Promise<TokenList> {
   try {
-    await validate(ValidationSchema.LIST, json)
+    await validate()
     return json
   } catch (error) {
     throw new Error(`Token list failed validation: ${error.message}`)
